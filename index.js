f90a2ee036fb1c0697e866f6d8c0d089c9774441
AndSinhorin <andressasinhorin@gmail.com>
2024-09-18 15:28:46 -0300

    trabalho

--- index.js
+++ index.js
@@ -21,6 +21,44 @@ let esquemaSeries = new mongoose.Schema({
 
 const Serie = mongoose.model('Series', esquemaSeries)
 
+async function criarSerie(nome, ano, genero, classificacao, diretor) {
+    try{
+        const novaSerie = new Serie({nome, ano, genero, classificacao, diretor});
+        return await novaSerie.save();
+    } catch (erro) {
+        console.error("Erro ao criar serie:", erro);
+        throw erro;
+    }
+}
+
+app.post("/series", async (req, res) => {
+    try {
+        const {nome, ano, genero, classificacao, diretor} = req.body;
+        const novaSerie = await criarSerie(nome, ano, genero, classificacao, diretor);
+        res.status(201).json({mensagem: "Serie adicionada com sucesso!", serie: novaSerie});
+    } catch (erro) {
+        res.status(500).json({mensagem: "Erro ao adicionar serie", erro: erro.message});
+    }
+});
+
+async function listarSeries() {
+    try {
+        return await Serie.find();
+    } catch (erro) {
+        console.error("Erro ao listar series: ", erro);
+        throw errro;
+    }
+}
+
+app.get("/series", async (req, res) => {
+    try {
+        const series = await listarSeries();
+        res.status(200).json(series);
+    } catch (erro) {
+        res.status(500).json({mensagem: "Erro ao listar series", erro: erro.message});
+    }
+})
+
     const port = 3000
 app.listen(port, () => {
     console.log(`Servidor rodando em http://localhost:${port}`)
